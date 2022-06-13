1bdd3d19af660064209d120439a212a2eec83c80
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2022-06-13 17:02:09 +0900

    scripts

--- scripts/cf_dense.sh
+++ scripts/cf_dense.sh
@@ -1,4 +1,5 @@
-for s in {0..4}
+#!/bin/bash
+for var in 0 1 2 3 4
 do
-    python train_drone_td3.py --rnn LSTM2 --tb_log --reward_norm --seed $s --gpu $1
+    python train_drone_td3.py --rnn LSTM2 --tb_log --reward_norm --seed $var --gpu $1
 done
